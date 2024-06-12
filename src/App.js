04267aa3d4d83599fb61c182f983d169e609f725
irem acar <iremmacar35@gmail.com>
2024-06-12 21:13:42 +0300

    props commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import { Container, Row,Col } from "reactstrap";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
 
 
+
 function App() {
   return (
     <div >
-      <Navi></Navi>
-      <CategoryList></CategoryList>
-      <ProductList></ProductList>
+      <Container>
+
+        <Row>
+          <Navi/>
+        </Row>
+
+        <Row>
+          <Col xs="3">
+            <CategoryList title="Category List"/>
+          </Col>
+
+          <Col xs="9">
+            <ProductList title="Product List"/>
+          </Col>
+
+          
+        </Row>
+
+      </Container>
+      
+      
     </div>
   );
 }
--- src/ProductList.js
+++ src/ProductList.js
@@ -5,7 +5,7 @@ export default class ProductList extends Component {
     return (
       <div>
         <h3>
-            Product list view
+        {this.props.title}
         </h3>
       </div>
     )
