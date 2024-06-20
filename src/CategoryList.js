7dc2f58361cc425326dac36688c171341732a1fe
irem acar <iremmacar35@gmail.com>
2024-06-20 19:52:47 +0300

    commit categories

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -3,11 +3,18 @@ import { ListGroup,ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
   
-  state = {categories : [
-      {categoryId:1,categoryName:"Beverages"},
-      {categoryId:2,categoryName:"Condiment"}
-    ]
-  } 
+  state = {categories : []
+  } ;
+
+  componentDidMount(){
+    this.getCategories();
+  }
+
+  getCategories = ()=>{
+    fetch("http://localhost:3000/categories")
+    .then(response=>response.json())
+    .then(data=>this.setState({categories:data}));;
+  }
   
   render() {
     return (
@@ -19,7 +26,8 @@ export default class CategoryList extends Component {
             this.state.categories.map(category=>(
               <ListGroupItem 
               onClick={() => this.props.changeCategory(category)} 
-              key ={category.categoryId}>
+              key ={category.id}
+              >
                 {category.categoryName}
               </ListGroupItem>
             ))
